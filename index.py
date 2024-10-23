eb2f56d4243371847cd5ef470224c71856cc542a
lightnew10 <64336831+lightnew10@users.noreply.github.com>
2024-10-23 16:34:56 +0200

    Ajout des images

--- index.py
+++ index.py
@@ -15,13 +15,23 @@ def loadFiles():
         if file.endswith(".md"):
             loadMD(file)
 
-
 def loadMD(file):
     full_file_path = os.path.join(path, file)
     with open(full_file_path, "r", encoding="utf-8") as f:
         content = f.read()
         html_body = markdown.markdown(content)
         event_association.append(file.replace(".md", ".html"))
+
+        splited = file.replace(".md", "").split('-')[-1]
+        dir_list = os.listdir(path)
+        image = None
+        for ff in dir_list:
+            if ff.endswith("webp"):
+                ffp = os.path.join(path, ff)
+                nameImage = ffp.replace(".webp", "").split("-")[-1]
+                if (nameImage == splited):
+                    image = ffp
+        html_body = html_body + f"""<img src="{image}" alt="">"""
         html_complete = f"""
             <!DOCTYPE html>
             <html lang="en">
